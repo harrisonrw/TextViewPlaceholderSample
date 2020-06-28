8687a6ae591fa1e5883a4570794aebf1bf838599
Robert Harrison <harrisonrw@gmail.com>
2020-06-28 15:47:27 -0700

    Initial Commit

---
+++ TextViewPlaceholderSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextViewPlaceholderSample
+//
+//  Created by Robert Harrison on 6/28/20.
+//  Copyright © 2020 RWH Technology, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
